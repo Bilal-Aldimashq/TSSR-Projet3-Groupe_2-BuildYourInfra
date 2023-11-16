a0e942b17d3f2faf141a7201b4b0d5c671bfed12
michaelc31 <144696421+michaelc31@users.noreply.github.com>
2023-11-16 17:12:37 +0100

    Add files via upload

---
+++ SuppressionAllUsers.ps1
@@ -0,0 +1,11 @@
+﻿##################################
+#                                #
+#   Suppression USER dans l'AD   #
+#                                #
+##################################
+
+### Main program
+
+Clear-Host
+$ADUsers = Get-ADUser -Filter *
+$ADUsers | where {$_.Name -ne "Administrator"} | `            where {$_.Name -ne "Guest"} | `            where {$_.Name -ne "DefaultAccount"} | `            Remove-ADUser -Confirm:$false
