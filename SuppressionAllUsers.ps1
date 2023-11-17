3ddb5006fe8b35d6dc46eda505818d66e298041b
michaelc31 <144696421+michaelc31@users.noreply.github.com>
2023-11-17 15:48:31 +0100

    Delete SuppressionAllUsers.ps1

--- SuppressionAllUsers.ps1
+++
@@ -1,11 +0,0 @@
-﻿##################################
-#                                #
-#   Suppression USER dans l'AD   #
-#                                #
-##################################
-
-### Main program
-
-Clear-Host
-$ADUsers = Get-ADUser -Filter *
-$ADUsers | where {$_.Name -ne "Administrator"} | `            where {$_.Name -ne "Guest"} | `            where {$_.Name -ne "DefaultAccount"} | `            Remove-ADUser -Confirm:$false
