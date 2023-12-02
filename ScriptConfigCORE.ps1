67cb95c0b5be07df8df539dcc1e2c57a8da71ce8
Bilal-Aldimashq <126762123+Bilal-Aldimashq@users.noreply.github.com>
2023-12-02 11:32:27 +0100

    Add Head and synopsis sections

--- ScriptConfigCORE.ps1
+++ ScriptConfigCORE.ps1
@@ -1,16 +1,28 @@
 ﻿<#
-Pour ce Script nous sommes partis sur un script avec arguments. Pour être sûr de ne pas en oublier lors du lancement du script nous les initiliserons avec un "read-host"
+#################################################################### HEAD ##########################################################################
+Nom: ConfigCORE.ps1
+Auteur: Mickaël C., Valentin A.
+Date: 30/11/2023
 
+Version: 1.0
+Date de modification:
+Par:
+Commentaires:
+####################################################################################################################################################
+################################################################### Synopsis #######################################################################
+ 
+Pour ce Script nous sommes partis sur un script avec arguments. Pour être sûr de ne pas en oublier lors du lancement du script, nous les initialiserons avec un "read-host".
 
 Le script se lance en demandant plusieurs renseignements qui serviront d'arguments, tels que le nom de serveur, @IP, @IP de la Gateway et le nom de domaine à rejoindre.
-Ensuite ce script va renommer le serveur, changer l'@IP en renseignant la gateway, elle défini l'adresse de serveur DNS 
-elle va installer les différents rôles et fonctionnalités sur un serveur Windows à l'aide de la cmdlet Install-WindowsFeature.
-elle installe un nouveau contrôleur de domaine Active Directory sur le serveur Windows,
-et récupère des informations sur un contrôleur de domaine spécifique dans Active Directory
-et redemarre le serveur afin d'appliquer les changements.
+Ensuite ce script va renommer le serveur, changer l'@IP en renseignant la gateway, définir l'adresse de serveur DNS, 
+ainsi qu'installer les différents rôles et fonctionnalités sur un serveur Windows à l'aide de la cmdlet Install-WindowsFeature.
+Il installe un nouveau contrôleur de domaine Active Directory sur le serveur Windows,
+récupère des informations sur un contrôleur de domaine spécifique dans Active Directory
+et redémarre le serveur afin d'appliquer les changements.
 
-
-il nous reste a traiter les erreurs, en regardant comment faire les verifs necessaires (verif IP, verif Nom,...) et synthaxer le script correctement.
+Améliorations:
+Reste a traiter les erreurs, en regardant comment faire les verifs necessaires (verif IP, verif Nom,...) et synthaxer le script correctement.
+####################################################################################################################################################
 #>
 
 
