81c26882c5927f26c089fa75677f31b25710490a
jeromedhauyre <146104077+jeromedhauyre@users.noreply.github.com>
2023-12-15 09:42:34 +0100

    Add files via upload
    script pour l'ajout des dossiers personnels dans un dossier partagé

---
+++ Scripts/ajout-d'un-dossier-perso.ps1
@@ -0,0 +1,14 @@
+﻿    $users = Get-ADUser -filter *
+
+    foreach ($user in $users)
+    {
+    $cheminDossierPartage = "\\SRVWINP3\PersonalFolders\$($user.SamAccountName)"
+       #ajouter de la ligne \srwin\personnalFolders\le nom de l'utilsateur
+        Set-ADUser $user -HomeDirectory $cheminDossierPartage -HomeDrive "I:"
+       #créé le nouveau repertoire pour le laison avec l'ajout de commande
+        New-Item $cheminDossierPartage -ItemType Directory 
+       # Ajoutez l'utilisateur au dossier avec des autorisations spécifiques (par exemple, contrôle total)
+        Add-NTFSAccess -Path $cheminDossierPartage -Account $($user.SamAccountName) -AccessRights FullControl
+
+        Write-Host "le dossier personnel à été crée et à été ajouté au lecteur logique I: et les droit d'acces ont ete ajouter" -ForegroundColor Green 
+    }
